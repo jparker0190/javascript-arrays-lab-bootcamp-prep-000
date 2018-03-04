c6fb24de802b9e18e632cd7aa753c68275fd193d
Joseph Parker <jparker010190@gmail.com>
2018-03-04 23:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ function prependKitten(name){
   kittens = [name,...kittens]
   return kittens
 }
-function removeFirstKitten(){
-  kittens = kittens.slice(1)
-  return kittens
-}
 function removeLastKitten(){
-  kittens = kittens.slice(0,kittens.length - 1)
-  return kittens
+  
 }
